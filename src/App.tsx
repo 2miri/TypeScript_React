b437e14ea51ed78f2710e23be7deb39081a8b54a
miri <miri@goodsoft.io>
2025-08-04 16:10:08 +0900

    컴포넌트 기본

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+
 export default function App() {
   return (
     <>
-      <h1>App Component</h1>
+      <Header />
+      <Home />
+      <Footer />
     </>
   );
 }
