399becbbdb237332adb1e05c5f5a2716122607a7
miri <miri@goodsoft.io>
2025-08-04 16:51:32 +0900

    [컴포넌트와 Props] 기본데이터 출력하기

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,25 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 
 export default function App() {
+  // 기본 자료형
+  const primitiveString = "Hello, World ! ";
+  const primitiveNumber = 42;
+  const primitiveBoolean = false;
+  const primitiveUndefinde = undefined;
+  const primitiveNull = null;
+  const primitiveSymbol = Symbol("mySymbol");
+  const primitiveBigInt = 9007199254740991n;
+
   return (
     <>
-      <Header />
-      <Main />
-      <Footer />
+      <h2>기본 자료형 출력</h2>
+      <p>문자열: {primitiveString}</p>
+      <p>숫자: {primitiveNumber}</p>
+      <p>논리형: {primitiveBoolean.toString()}</p>
+      <p>undefined: {String(primitiveUndefinde)}</p>
+      <p>null: {String(primitiveNull)}</p>
+      <p>symbol: {String(primitiveSymbol)}</p>
+      <p>BigInt: {primitiveBigInt}</p>
     </>
   );
 }
