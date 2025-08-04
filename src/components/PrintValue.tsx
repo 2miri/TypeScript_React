7bc7dd2c812b4b3cededadc9e1efe31103f81622
miri <miri@goodsoft.io>
2025-08-04 17:36:08 +0900

    [컴포넌트와 Props] 타입 정의하는 방법

--- src/components/PrintValue.tsx
+++ src/components/PrintValue.tsx
@@ -1,3 +1,16 @@
+export default function PrintValue(props: PrintValueProps) {
+  return (
+    <>
+      <p>number: {props.numberValue}</p>
+      <p>string: {props.stringValue}</p>
+      <p>boolean: {props.booleanValue.toString()}</p>
+      <p>array: {props.arrayValue}</p>
+      <p>object: {JSON.stringify(props.objectValue)}</p>
+      <p>function: {props.handleClick.toString()} </p>
+    </>
+  );
+}
+
 // 비구조화 할당 방법1
 // export default function PrintValue({
 //   numberValue,
@@ -27,30 +40,30 @@
 // }
 
 // 비구조화 할당 방법2
-export default function PrintValue(props: {
-  numberValue: number;
-  stringValue: string;
-  booleanValue: true | false;
-  arrayValue: number[];
-  objectValue: { name: string; age: number };
-  handleClick: () => void;
-}) {
-  const {
-    numberValue,
-    stringValue,
-    booleanValue,
-    arrayValue,
-    objectValue,
-    handleClick,
-  } = props;
-  return (
-    <>
-      <p>number: {numberValue}</p>
-      <p>string: {stringValue}</p>
-      <p>boolean: {booleanValue.toString()}</p>
-      <p>array: {arrayValue}</p>
-      <p>object: {JSON.stringify(objectValue)}</p>
-      <p>function: {handleClick.toString()}</p>
-    </>
-  );
-}
+// export default function PrintValue(props: {
+//   numberValue: number;
+//   stringValue: string;
+//   booleanValue: true | false;
+//   arrayValue: number[];
+//   objectValue: { name: string; age: number };
+//   handleClick: () => void;
+// }) {
+//   const {
+//     numberValue,
+//     stringValue,
+//     booleanValue,
+//     arrayValue,
+//     objectValue,
+//     handleClick,
+//   } = props;
+//   return (
+//     <>
+//       <p>number: {numberValue}</p>
+//       <p>string: {stringValue}</p>
+//       <p>boolean: {booleanValue.toString()}</p>
+//       <p>array: {arrayValue}</p>
+//       <p>object: {JSON.stringify(objectValue)}</p>
+//       <p>function: {handleClick.toString()}</p>
+//     </>
+//   );
+// }
