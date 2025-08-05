754d3a6b464b976799bd4b12d4e63469640eaf83
miri <miri@goodsoft.io>
2025-08-05 10:42:07 +0900

    [컴포넌트와 이벤트] 이벤트 객체 배우기 - 2

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,23 +1,14 @@
-export default function Button() {
-  const handleClick = (
+export default function Button({
+  handleClick,
+}: {
+  handleClick: (
     message: string,
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    console.log(message);
-    console.log(event);
-  };
-
-  const handleClick2 = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    console.log("handleClick2");
-    console.log(event);
-  };
-
+  ) => void;
+}) {
   return (
     <>
-      <button onClick={(event) => handleClick("click!", event)}>클릭</button>
-      <button onClick={handleClick2}>클릭2</button>
+      <button onClick={(event) => handleClick("hello", event)}>클릭</button>
     </>
   );
 }
