0c771a9ef61e8b95981a47b7855ea9cbc081613f
miri <miri@goodsoft.io>
2025-08-05 11:15:51 +0900

    [컴포넌트와 이벤트] 이벤트 전파 - 버블링

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,9 @@
-import Button from "./components/ui/Button";
+import Table from "./components/Table";
 
 export default function App() {
-  const handleClick = (
-    message: string,
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    console.log(message);
-    console.log(event);
-    // 방법1
-    event.currentTarget.innerText = message;
-    // 방법2 - 비추
-    // (event.target as HTMLButtonElement).innerText = message;
-  };
   return (
     <>
-      <Button handleClick={handleClick} />
+      <Table />
     </>
   );
 }
