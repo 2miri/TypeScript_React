a333c3ecabcb5ba4e347e381e0a4e3c9c25b694f
miri <miri@goodsoft.io>
2025-08-05 16:09:15 +0900

    [과제] 카운터 앱 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import { useState } from "react";
 
 export default function App() {
   const [count, setCount] = useState(0);
-  const handleIncrement = () => {
+
+  function handleIncrement() {
     setCount((count) => count + 1);
-    setCount((count) => count + 1);
-    setCount((count) => count + 1);
-  };
+  }
+
+  function handleDecrement() {
+    setCount((count) => count - 1);
+  }
+
+  function handleReset() {
+    setCount(0);
+  }
+
   return (
     <>
-      <h1>Count: {count}</h1>
-      <button onClick={handleIncrement}>increment</button>
+      <h1>Count : {count}</h1>
+      <button onClick={handleIncrement}>증가</button>
+      <button onClick={handleDecrement}>감소</button>
+      <button onClick={handleReset}>리셋</button>
     </>
   );
 }
