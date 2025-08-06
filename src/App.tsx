860e6de224a4b55610d6cc924df311e406e2442a
miri <miri@goodsoft.io>
2025-08-06 09:14:08 +0900

    [컴포넌트 상태] 배열 상태 다루기

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,23 @@
 import { useState } from "react";
 
 export default function App() {
-  const [userInfo, setUserInfo] = useState({
-    name: "jack",
-    age: 20,
-    gender: "male",
-    contact: {
-      email: "jack@example.com",
-      phone: "123-456-7890",
-    },
-    address: {
-      home: {
-        street: "123 Main St",
-        city: "New York",
-        zipCode: "10001",
-      },
-      office: {
-        street: "456 Business Ave",
-        city: "New York",
-        zipCode: "10002",
-      },
-    },
-  });
+  const [fruits, setFruits] = useState(["apple", "banana", "orange"]);
+  const handleAddFruit = () => {
+    // melon 추가
+    // setFruits((fruits) => [...fruits, "melon"]);
 
-  const handleUpdateUserInfo = () => {
-    setUserInfo((userInfo) => ({
-      ...userInfo,
-      name: "mike",
-      contact: {
-        ...userInfo.contact,
-        email: "mike@naver.com",
-      },
-      address: {
-        ...userInfo.address,
-        home: {
-          ...userInfo.address.home,
-          street: "111 main st",
-        },
-      },
-    }));
+    // apple을 grape로 변경
+    // setFruits((fruits) =>
+    //   fruits.map((fruit) => (fruit === "apple" ? "grape" : fruit))
+    // );
+
+    // 두번째에 grape 추가
+    setFruits((fruits) => [...fruits.slice(0, 1), "grape", ...fruits.slice(1)]);
   };
-
   return (
     <>
-      <pre>{JSON.stringify(userInfo, null, 2)}</pre>
-      <button onClick={handleUpdateUserInfo}>UpdateUserInfo</button>
+      <p>{fruits.join(", ")}</p>
+      <button onClick={handleAddFruit}>Add Fruit</button>
     </>
   );
 }
