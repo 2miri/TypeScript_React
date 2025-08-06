cabd7de97a13fa4701609c37f5a4776ff4d76564
miri <miri@goodsoft.io>
2025-08-06 15:51:51 +0900

    [반복 렌더링과 조건부 렌더링] key 속성 고유하게 하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,32 @@
-import FruitList from "./components/FruitList";
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export default function App() {
+  const items = ["apple", "banana", "orange"];
+  const [fruits, setFruits] = useState(
+    items.map((item) => ({
+      id: uuidv4(),
+      value: item,
+    }))
+  );
+
   return (
     <>
-      <FruitList />
+      <h1>Fruits Lists</h1>
+      <ul>
+        {fruits.map((fruit) => (
+          <li key={fruit.id}>
+            <input type="text" placeholder={fruit.value} />
+          </li>
+        ))}
+      </ul>
+      <button
+        onClick={() =>
+          setFruits((fruits) => [{ id: uuidv4(), value: "grape" }, ...fruits])
+        }
+      >
+        추가
+      </button>
     </>
   );
 }
