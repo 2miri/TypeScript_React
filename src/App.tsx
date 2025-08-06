f71ac774b5672384fd42566b85d4a3e76415e6e4
miri <miri@goodsoft.io>
2025-08-06 17:33:15 +0900

    [컴포넌트 스타일링] 인라인 스타일

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import RecipeList from "./components/RecipeList";
+import Inline from "./components/Inline";
 
+// 인라인 스타일 방법
 export default function App() {
+  const isLogin = true;
+  const h1Style = { color: isLogin ? "red" : "blue", fontSize: "25px" };
   return (
     <>
-      <RecipeList />
+      <Inline h1Style={h1Style} />
     </>
   );
 }
