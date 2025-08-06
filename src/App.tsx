d4e6a29b7ba922b09ac01383f54d118c2867cd1e
miri <miri@goodsoft.io>
2025-08-06 11:12:46 +0900

    [컴포넌트 상태] useReducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import Count from "./components/Count";
+import { useReducer } from "react";
+import counterReducer from "./reducer/counterReducer";
+
 export default function App() {
+  const [count, countDispatch] = useReducer(counterReducer, 0);
+
   return (
     <>
-      <Count />
+      <h1>Count: {count}</h1>
+      <button onClick={() => countDispatch({ type: "decrement" })}>감소</button>
+      <button onClick={() => countDispatch({ type: "reset" })}>리셋</button>
+      <button onClick={() => countDispatch({ type: "increment" })}>증가</button>
     </>
   );
 }
---
+++ src/reducer/counterReducer.ts
@@ -0,0 +1,12 @@
+export default function reducer(count: number, action: { type: string }) {
+  switch (action.type) {
+    case "increment":
+      return count + 1;
+    case "decrement":
+      return count - 1;
+    case "reset":
+      return 0;
+    default:
+      return count;
+  }
+}
