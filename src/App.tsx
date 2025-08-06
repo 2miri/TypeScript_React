2e9858d7f0db94ca90539435e31a3c22250a2416
miri <miri@goodsoft.io>
2025-08-06 15:03:25 +0900

    [반복 렌더링과 조건부 렌더링] 조건부 렌더링 - && 연산자

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import LoginPanel from "./components/LoginPanel";
-
 export default function App() {
-  return <LoginPanel />;
+  const isLogin = true;
+  return (
+    <>
+      {isLogin && <h1>true</h1>}
+      {!isLogin && <h1>false</h1>}
+    </>
+  );
 }
