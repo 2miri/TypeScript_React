571ea900edcfa097b4f3f9cb9daeb65d4ef5a8d4
miri <miri@goodsoft.io>
2025-08-07 16:47:49 +0900

    [컴포넌트 스타일링] tailwind css - 조건부 스타일

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
+import { twMerge } from "tailwind-merge";
+
 export default function App() {
+  const isRed = false;
   return (
     <>
       <div className="flex flex-col items-center justify-center min-h-screen space-y-4">
-        <button className="button"> click me</button>
         <button
-          className="bg-green-500 text-white py-3 px-5 rounded 
-        border-none cursor-pointer transition-colors duration-300 hover:bg-green-600 "
+          className={twMerge(
+            `bg-green-500 text-white py-3 px-5 rounded  border-none 
+            cursor-pointer transition-colors duration-300
+             hover:bg-green-600`,
+            isRed && "bg-rose-500"
+          )}
         >
           click me
         </button>
-        <button className="btns">click me</button>
       </div>
     </>
   );
