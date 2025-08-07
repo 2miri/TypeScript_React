529d26692d180289ac05de9b5b61505ad50ecd0d
miri <miri@goodsoft.io>
2025-08-07 13:23:48 +0900

    [컴포넌트 스타일링] styled-components (확장)

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,31 @@ const Title = styled.h1<{ $color: string; $decoration: string }>`
   color: ${(props) => props.$color};
   text-decoration: ${(props) => props.$decoration};
 `;
+
+const BigTitle = styled(Title)`
+  font-size: 50px;
+`;
+
 const Wrapper = styled.section`
   padding: 2rem;
   border: 1px solid red;
 `;
+
+const BlueBorderWrapper = styled(Wrapper)`
+  border-color: blue;
+`;
+
 export default function App() {
   return (
     <>
-      <Wrapper>
+      <BlueBorderWrapper>
         <Title $color="blue" $decoration="line-through">
           Hello, ReactJS!
         </Title>
-      </Wrapper>
+        <BigTitle $color="purple" $decoration="none">
+          hello, big title!
+        </BigTitle>
+      </BlueBorderWrapper>
     </>
   );
 }
