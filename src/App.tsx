882b579b794056f2c6156e478a80a40f7b28cfba
miri <miri@goodsoft.io>
2025-08-08 11:34:22 +0900

    [컴포넌트 스타일링] 이미지 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import Calculator from "./components/Calculator";
-
+import sample from "./assets/images/cat.jpg";
 export default function App() {
   return (
     <>
-      <Calculator />
+      <img src="/images/cat.jpg" alt="퍼블릭 고양이" />
+      <img src="/src/assets/images/cat.jpg" alt="src 고양이" />
+      <img src={sample} alt="src 고양이" />
     </>
   );
 }
--- src/components/Greeting.tsx
+++ src/components/Greeting.tsx
@@ -1,6 +1,6 @@
 import GuestView from "./GuestView";
-import UserInfoView from "./UserInfoView";
-import UserView from "./UserView";
+// import UserInfoView from "./UserInfoView";
+// import UserView from "./UserView";
 
 export default function Greeting({ isLoggedIn }: { isLoggedIn: boolean }) {
   if (isLoggedIn) return null;
