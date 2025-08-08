32d8d11d755a1ce1a33ac3a3c8f10d93af9cf943
miri <miri@goodsoft.io>
2025-08-08 11:43:07 +0900

    [컴포넌트 스타일링] 이미지 렌더링 (CSS)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import sample from "./assets/images/cat.jpg";
+import cat from "./assets/images/cat.jpg";
 export default function App() {
   return (
     <>
-      <img src="/images/cat.jpg" alt="퍼블릭 고양이" />
-      <img src="/src/assets/images/cat.jpg" alt="src 고양이" />
-      <img src={sample} alt="src 고양이" />
+      {/* <div className="bg"></div> */}
+      <div className="bg-[url(https://cdn.pixabay.com/photo/2025/07/31/20/00/woman-9747618_1280.jpg)] w-[400px] h-[400px]"></div>
+      <div className="bg-[url(/images/cat.jpg)] w-[400px] h-[400px]"></div>
+      <div
+        className={`w-[400px] h-[400px]`}
+        style={{ backgroundImage: `url(${cat})` }}
+      ></div>
     </>
   );
 }
