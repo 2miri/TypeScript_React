2b52fb01de95593d5276abd1e20a2d3ec219a0f6
miri <miri@goodsoft.io>
2025-08-12 11:23:34 +0900

    [폼 다루기] 벨리데이션 - 커스텀

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import useInput from "./hooks/useInput";
+import {
+  validateEmail,
+  validateName,
+  validatePassword,
+} from "./utils/validation";
 
 export default function App() {
   const { value: email, handleValueChange: handleEmailChange } = useInput("");
@@ -8,6 +13,27 @@ export default function App() {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    // 이메일 검증
+    const emailError = validateEmail(email);
+    if (emailError) {
+      alert(emailError);
+      return;
+    }
+
+    // 비밀번호 검증
+    const passwordError = validatePassword(password);
+    if (passwordError) {
+      alert(passwordError);
+      return;
+    }
+
+    // 이름 검증
+    const nameError = validateName(name);
+    if (nameError) {
+      alert(nameError);
+      return;
+    }
+
     console.log({ email, password, name });
   };
 
@@ -19,23 +45,18 @@ export default function App() {
           placeholder="email"
           value={email}
           onChange={handleEmailChange}
-          required
         />
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={handlePasswordChange}
-          minLength={4}
-          maxLength={6}
-          required
         />
         <input
           type="text"
           placeholder="name"
           value={name}
           onChange={handleNameChange}
-          required
         />
         <button type="submit">제출</button>
       </form>
