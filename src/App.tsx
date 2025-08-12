cfbf1b1e1b2d31a9afc2cd8ab507d77f385eb267
miri <miri@goodsoft.io>
2025-08-12 11:33:50 +0900

    [폼 다루기] 벨리데이션 - 커스텀 훅 통합

--- src/App.tsx
+++ src/App.tsx
@@ -6,29 +6,37 @@ import {
 } from "./utils/validation";
 
 export default function App() {
-  const { value: email, handleValueChange: handleEmailChange } = useInput("");
-  const { value: password, handleValueChange: handlePasswordChange } =
-    useInput("");
-  const { value: name, handleValueChange: handleNameChange } = useInput("");
+  const {
+    value: email,
+    handleValueChange: handleEmailChange,
+    error: emailError,
+  } = useInput("", validateEmail);
+  const {
+    value: password,
+    handleValueChange: handlePasswordChange,
+    error: passwordError,
+  } = useInput("", validatePassword);
+  const {
+    value: name,
+    handleValueChange: handleNameChange,
+    error: nameError,
+  } = useInput("", validateName);
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // 이메일 검증
-    const emailError = validateEmail(email);
     if (emailError) {
       alert(emailError);
       return;
     }
 
     // 비밀번호 검증
-    const passwordError = validatePassword(password);
     if (passwordError) {
       alert(passwordError);
       return;
     }
 
     // 이름 검증
-    const nameError = validateName(name);
     if (nameError) {
       alert(nameError);
       return;
@@ -45,19 +53,25 @@ export default function App() {
           placeholder="email"
           value={email}
           onChange={handleEmailChange}
+          required
         />
+        {emailError && <p>{emailError}</p>}
         <input
           type="password"
           placeholder="password"
           value={password}
           onChange={handlePasswordChange}
+          required
         />
+        {passwordError && <p>{passwordError}</p>}
         <input
           type="text"
           placeholder="name"
           value={name}
           onChange={handleNameChange}
+          required
         />
+        {nameError && <p>{nameError}</p>}
         <button type="submit">제출</button>
       </form>
     </>
