8b56c5fef20e8850acba5eb1a9817bb7bcffa244
miri <miri@goodsoft.io>
2025-08-12 17:29:15 +0900

    [할 일 관리 앱 (TODO LIST)] 할 일 등록 기능 구현하기

--- src/components/TodoEditor.tsx
+++ src/components/TodoEditor.tsx
@@ -1,15 +1,28 @@
+import React, { useState } from "react";
 import Button from "./html/Button";
 import Input from "./html/Input";
 
-export default function TodoEditor() {
+export default function TodoEditor({
+  addTodo,
+}: {
+  addTodo: (text: string) => void;
+}) {
+  const [text, setText] = useState("");
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (text.trim() === "") return;
+    addTodo(text);
+    setText("");
+  };
   return (
     <>
-      <form className="todo__form">
+      <form className="todo__form" onSubmit={handleSubmit}>
         <div className="todo__editor">
           <Input
             type="text"
             className="todo__input"
             placeholder="Enter Todo List"
+            onChange={(e) => setText(e.target.value)}
           />
           <Button className="todo__button" type="submit">
             Add
