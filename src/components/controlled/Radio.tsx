5dc07da5a80c1c837a53e7d959b28a1bab7a0f6d
miri <miri@goodsoft.io>
2025-08-12 09:58:17 +0900

    [폼 다루기] 비제어 컴포넌트 - 라디오

--- src/components/controlled/Radio.tsx
+++ src/components/controlled/Radio.tsx
@@ -1,83 +1,37 @@
-import { useState } from "react";
+import type React from "react";
+import { useRef } from "react";
 
 export default function Radio() {
-  const [formState, setFormState] = useState({
-    option: "option1",
-    colorOption: "option1",
-  });
-  const handleFormStateChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setFormState((formState) => ({
-      ...formState,
-      [e.target.name]: e.target.value,
-    }));
+  const formElRedf = useRef<HTMLFormElement>(null);
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(formElRedf.current!);
+    console.log(formData.get("option"));
   };
   return (
-    <section>
+    <form ref={formElRedf} onSubmit={handleSubmit}>
       <div>
         <label>
-          <input
-            type="radio"
-            name="option"
-            value="option1"
-            checked={formState.option === "option1"}
-            onChange={handleFormStateChange}
-          />
+          <input type="radio" name="option" value="option1" defaultChecked />
           옵션 1
         </label>
+      </div>
+
+      <div>
         <label>
-          <input
-            type="radio"
-            name="option"
-            value="option2"
-            checked={formState.option === "option2"}
-            onChange={handleFormStateChange}
-          />
+          <input type="radio" name="option" value="option2" />
           옵션 2
         </label>
+      </div>
+
+      <div>
         <label>
-          <input
-            type="radio"
-            name="option"
-            value="option3"
-            checked={formState.option === "option3"}
-            onChange={handleFormStateChange}
-          />
+          <input type="radio" name="option" value="option3" />
           옵션 3
         </label>
       </div>
 
-      <div>
-        <label>
-          <input
-            type="radio"
-            name="colorOption"
-            value="option1"
-            checked={formState.colorOption === "option1"}
-            onChange={handleFormStateChange}
-          />
-          컬러 옵션 1
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="colorOption"
-            value="option2"
-            checked={formState.colorOption === "option2"}
-            onChange={handleFormStateChange}
-          />
-          컬러 옵션 2
-        </label>
-        <label>
-          <input
-            type="radio"
-            name="colorOption"
-            value="option3"
-            checked={formState.colorOption === "option3"}
-            onChange={handleFormStateChange}
-          />
-          컬러 옵션 3
-        </label>
-      </div>
-    </section>
+      <button type="submit">제출</button>
+    </form>
   );
 }
