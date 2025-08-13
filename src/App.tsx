5586ca7ec75aa50b79dd258cd11f4016584f0d90
miri <miri@goodsoft.io>
2025-08-13 13:51:31 +0900

    lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,21 @@
-import { useMemo, useState } from "react";
-
-const initialItems = new Array(29_999_999).fill(0).map((_, i) => {
-  return {
-    id: i,
-    selected: i === 29_999_998,
-  };
-});
+import { lazy, useState } from "react";
+// import ChildA from "./components/ChildA";
+// import ChildB from "./components/ChildB";
+const ChildA = lazy(() => import("./components/ChildA"));
+const ChildB = lazy(() => import("./components/ChildB"));
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
-  // 불필요한 연산을 만들어준 것
-  // 3000만개의 배열 데이터를 렌더링 마다 재생성하고 있음
-  const selectItems = useMemo(
-    () => initialItems.find((item) => item.selected),
-    []
-  );
+  const [isShow, setIsShow] = useState(false);
   return (
     <>
-      <h1>Count: {count}</h1>
-      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
-        증가
-      </button>
-      <p>{selectItems?.id}</p>
+      <button onClick={() => setIsShow((isShow) => !isShow)}>토글</button>
+      <h1>App Component</h1>
+      {isShow && (
+        <>
+          <ChildA />
+          <ChildB />
+        </>
+      )}
     </>
   );
 }
--- src/components/ChildA.tsx
+++ src/components/ChildA.tsx
@@ -1,11 +1,7 @@
 import React, { useState } from "react";
 import ChildB from "./ChildB";
 
-export default React.memo(function ChildA({
-  increment,
-}: {
-  increment: () => void;
-}) {
+export default React.memo(function ChildA() {
   console.log("childA");
   return (
     <>
