4f194957b864fe795173fdbd15368e5bb41332a8
miri <miri@goodsoft.io>
2025-08-13 16:13:49 +0900

    [전역 상태 관리 - Context API] UserProfile 초기 제공 코드 살펴보기

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,9 @@
-import Count from "./components/Count";
-import CountOutside from "./components/CountOutside";
-import Theme from "./components/Theme";
-import ThemeButton from "./components/ThemeButton";
-import CounterProvider from "./context/counter/CounterProvider";
-import ThemeProvider from "./context/theme/ThemeProvider";
+import UserSettings from "./components/UserSettings";
 
 export default function App() {
   return (
     <>
-      <ThemeProvider>
-        <CounterProvider>
-          <Count />
-          <CountOutside />
-          <Theme />
-          <ThemeButton />
-        </CounterProvider>
-      </ThemeProvider>
+      <UserSettings />
     </>
   );
 }
