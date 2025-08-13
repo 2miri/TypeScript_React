e5a42782a69e0e2ab603957bd2c82e577d99b0e8
miri <miri@goodsoft.io>
2025-08-13 10:12:09 +0900

    [사이드 이펙트와 컴포넌트 최적화] useEffect 실습

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import Todo from "./components/Todo";
+import { useState } from "react";
+import Count from "./components/Count";
 
 export default function App() {
+  const [isShow, setIsShow] = useState(false);
   return (
     <>
-      <Todo />
+      <button onClick={() => setIsShow((isShow) => !isShow)}>노출 변경</button>
+      {isShow && <Count />}
     </>
   );
 }
--- src/components/Count.tsx
+++ src/components/Count.tsx
@@ -1,15 +1,27 @@
-import { useReducer } from "react";
-import counterReducer from "../reducer/counterReducer";
-
-import CountDisplay from "./CountDisplay";
-import CountButton from "./CountButton";
+import { useEffect, useState } from "react";
 
 export default function Count() {
-  const [count, countDispatch] = useReducer(counterReducer, 0);
+  const [count, setCount] = useState(0);
+
+  // 컴포넌트가 생성될 때만 호출됩니다.
+  useEffect(() => {
+    console.log("카운트 컴포넌트 생성");
+
+    // 컴포넌트가 삭제(언마운트) 될 때만 호출하는 코드는...
+    return () => {
+      console.log("카운트 컴포넌트 삭제");
+    };
+  }, []);
+
+  // 컴포넌트의 상태가 변경 되었을 때만 호출하려면 ?
+  useEffect(() => {
+    console.log("count change : " + count);
+  }, [count]);
+
   return (
     <>
-      <CountDisplay count={count} />
-      <CountButton countDispatch={countDispatch} />
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount((count) => count + 1)}>증가</button>
     </>
   );
 }
