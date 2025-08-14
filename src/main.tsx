1bb9195366c5a6af838ef1c18b8cd3f689519858
miri <miri@goodsoft.io>
2025-08-14 13:19:47 +0900

    [전역 상태 관리 - Redux Toolkit] Redux Store 생성

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
-// import "./styles/index.css";
-import "./styles/todo.css";
+import "./styles/index.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
+// import "./styles/todo.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
