9515247cc4cf52a7e6ceffea49d3ddfb304d24b9
miri <miri@goodsoft.io>
2025-08-14 14:47:44 +0900

    [전역 상태 관리 - Redux Toolkit] Redux Thunk

--- src/components/CountButton.tsx
+++ src/components/CountButton.tsx
@@ -2,13 +2,15 @@ import { useDispatch } from "react-redux";
 import {
   decrement,
   increment,
+  incrementAsync,
   reset,
 } from "../store/features/counter/counterSlice";
+import type { AppDispatch } from "../store/store";
 
 export default function CountButton() {
   console.log("CountButton");
 
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
   return (
     <>
       <button onClick={() => dispatch(decrement(5))}>감소</button>
@@ -16,6 +18,7 @@ export default function CountButton() {
       <button onClick={() => dispatch(increment({ num1: 5, num2: 3 }))}>
         증가
       </button>
+      <button onClick={() => dispatch(incrementAsync(10))}>증가2</button>
     </>
   );
 }
