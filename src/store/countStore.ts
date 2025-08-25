1ac6d151cb9e921e00184ea9d6d85b98af8b483b
miri <miri@goodsoft.io>
2025-08-25 10:47:10 +0900

    [전역 상태 관리 - Zustand] zustand devtools

--- src/store/countStore.ts
+++ src/store/countStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import {
   createJSONStorage,
+  devtools,
   persist,
   subscribeWithSelector,
 } from "zustand/middleware";
@@ -15,29 +16,34 @@ type CountStore = {
 
 // use***Store
 export const useCountStore = create<CountStore>()(
-  subscribeWithSelector(
-    persist(
-      immer((set) => ({
-        count: 0,
-        decrement: () =>
-          set((state) => {
-            state.count -= 1;
-          }),
-        increment: async (amount: number, amount2: number) => {
-          // await new Promise((resolve) => setTimeout(resolve, 1000));
-          set((state) => {
-            state.count += amount + amount2;
-          });
-        },
-        reset: () =>
-          set((state) => {
-            state.count = 0;
-          }),
-      })),
-      {
-        name: "count-storage",
-        storage: createJSONStorage(() => sessionStorage), // 세션 스토리지
-      }
-    )
+  devtools(
+    subscribeWithSelector(
+      persist(
+        immer((set) => ({
+          count: 0,
+          decrement: () =>
+            set((state) => {
+              state.count -= 1;
+            }),
+          increment: async (amount: number, amount2: number) => {
+            // await new Promise((resolve) => setTimeout(resolve, 1000));
+            set((state) => {
+              state.count += amount + amount2;
+            });
+          },
+          reset: () =>
+            set((state) => {
+              state.count = 0;
+            }),
+        })),
+        {
+          name: "count-storage",
+          storage: createJSONStorage(() => sessionStorage), // 세션 스토리지
+        }
+      )
+    ),
+    {
+      enabled: import.meta.env.MODE === "development",
+    }
   )
 );
