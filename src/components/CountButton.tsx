1c71af546c2fc0bf0e0dce3fa01e107a1fa63179
miri <miri@goodsoft.io>
2025-08-25 10:20:22 +0900

    [전역 상태 관리 - Zustand] zustand 사용하기

--- src/components/CountButton.tsx
+++ src/components/CountButton.tsx
@@ -1,9 +1,15 @@
+import { useCountStore } from "../store/countStore";
+
 export default function CountButton() {
+  console.log("CountButton");
+  const increment = useCountStore((state) => state.increment);
+  const decrement = useCountStore((state) => state.decrement);
+  const reset = useCountStore((state) => state.reset);
   return (
     <>
-      <button>감소</button>
-      <button>리셋</button>
-      <button>증가</button>
+      <button onClick={decrement}>감소</button>
+      <button onClick={reset}>리셋</button>
+      <button onClick={increment}>증가</button>
     </>
   );
 }
--- src/components/CountDisplay.tsx
+++ src/components/CountDisplay.tsx
@@ -1,7 +1,10 @@
+import { useCountStore } from "../store/countStore";
+
 export default function CountDisplay() {
+  const count = useCountStore((state) => state.count);
   return (
     <>
-      <h1>Count: 0</h1>
+      <h1>Count: {count}</h1>
     </>
   );
 }
