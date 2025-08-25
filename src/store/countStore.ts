d756b90330121dcf74137590d3882824d47cf45c
miri <miri@goodsoft.io>
2025-08-25 10:31:36 +0900

    [전역 상태 관리 - Zustand] zustand persist

--- src/store/countStore.ts
+++ src/store/countStore.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type CountStore = {
   count: number;
@@ -8,12 +9,20 @@ type CountStore = {
 };
 
 // use***Store
-export const useCountStore = create<CountStore>((set) => ({
-  count: 0,
-  decrement: () => set((state) => ({ count: state.count - 1 })),
-  increment: async (amount: number, amount2: number) => {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    set((state) => ({ count: state.count + amount + amount2 }));
-  },
-  reset: () => set({ count: 0 }),
-}));
+export const useCountStore = create<CountStore>()(
+  persist(
+    (set) => ({
+      count: 0,
+      decrement: () => set((state) => ({ count: state.count - 1 })),
+      increment: async (amount: number, amount2: number) => {
+        // await new Promise((resolve) => setTimeout(resolve, 1000));
+        set((state) => ({ count: state.count + amount + amount2 }));
+      },
+      reset: () => set({ count: 0 }),
+    }),
+    {
+      name: "count-storage",
+      storage: createJSONStorage(() => sessionStorage), // 세션 스토리지
+    }
+  )
+);
