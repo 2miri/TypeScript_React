5678aec3b498d43f3acc9c559442f189fe7019da
miri <miri@goodsoft.io>
2025-08-26 16:47:09 +0900

    [데이터 통신 심화] useOptimistic 실전 - 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import axios from "axios";
 import { Heart } from "lucide-react";
-import { useEffect, useState } from "react";
+import {
+  startTransition,
+  useEffect,
+  useOptimistic,
+  useRef,
+  useState,
+} from "react";
 
 interface Posts {
   id: number;
@@ -8,16 +14,42 @@ interface Posts {
 }
 
 export default function App() {
+  const controller = useRef<AbortController | null>(null);
   const [posts, setPosts] = useState<Posts[]>([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [optimisticPosts, addOptimisticPosts] = useOptimistic(
+    posts,
+    (statePosts, id) => {
+      return statePosts.map((statePost) =>
+        statePost.id === id
+          ? { ...statePost, isLike: !statePost.isLike }
+          : statePost
+      );
+    }
+  );
 
-  const updateLike = async (id: number, isLike: boolean) => {
-    const { data } = await axios.patch(`http://localhost:3000/posts/${id}`, {
-      isLike: !isLike,
+  const updateLike = (id: number, isLike: boolean) => {
+    controller.current?.abort();
+    controller.current = new AbortController();
+    startTransition(async () => {
+      addOptimisticPosts(id);
+      try {
+        const { data } = await axios.patch(
+          `http://localhost:3000/posts/${id}`,
+          {
+            isLike: !isLike,
+          },
+          {
+            signal: controller.current?.signal,
+          }
+        );
+        setPosts((posts) =>
+          posts.map((post) => (post.id === data.id ? data : post))
+        );
+      } catch (e) {
+        console.error("에러 : ", e);
+      }
     });
-    setPosts((posts) =>
-      posts.map((post) => (post.id === data.id ? data : post))
-    );
   };
 
   useEffect(() => {
@@ -26,7 +58,6 @@ export default function App() {
         setIsLoading(true);
         const { data } = await axios.get("http://localhost:3000/posts");
         setPosts(data);
-        setIsLoading(false);
       } catch (e) {
         console.error("에러 발생 : ", e);
       } finally {
@@ -41,7 +72,7 @@ export default function App() {
     <>
       {/* fill: 'none', stroke: 'currentColor' */}
       {/* fill: 'rgb(255,0,0)', stroke: 'rgb(255,0,0)' */}
-      {posts.map((post) => (
+      {optimisticPosts.map((post) => (
         <Heart
           key={post.id}
           fill={post.isLike ? "rgb(255, 0, 0)" : "none"}
