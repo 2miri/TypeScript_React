c0d9bf832d01c0d57a13fc83953f92e2f2bb7abf
miri <miri@goodsoft.io>
2025-08-26 13:42:12 +0900

    [데이터 통신] Post Browser 검색

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -4,31 +4,43 @@ import { axiosInstance } from "../api/axios";
 import LoadingState from "./LoadingState";
 import ErrorState from "./ErrorState";
 import { usePostStore } from "../store/postStore";
+import NoData from "./NoData";
 
 export default function PostList() {
   const [posts, setPost] = useState<Posts[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState("");
 
+  const term = usePostStore((state) => state.term);
   const currentPage = usePostStore((state) => state.currentPage);
   const limit = usePostStore((state) => state.limit);
+
   useEffect(() => {
+    const controller = new AbortController();
     const fetchPosts = async () => {
       setIsLoading(true);
       setError("");
       try {
         const { data } = await axiosInstance.get(
-          `/posts?_page=${currentPage}&_limit=${limit}`
+          `/posts?q=${encodeURIComponent(
+            term
+          )}&_page=${currentPage}&_limit=${limit}`,
+          {
+            signal: controller.signal,
+          }
         );
         setPost(data);
       } catch (e) {
-        setError(e instanceof Error ? e.message : "unknown error");
+        if (e instanceof Error && e.name !== "CanceledError")
+          setError(e instanceof Error ? e.message : "unknown error");
       } finally {
-        setIsLoading(false);
+        if (!controller.signal.aborted) setIsLoading(false);
       }
     };
     fetchPosts();
-  }, [currentPage, limit]);
+
+    return () => controller.abort();
+  }, [currentPage, limit, term]);
   return (
     <div className="mb-8">
       {/* 데이터가 없을 때 */}
@@ -42,12 +54,14 @@ export default function PostList() {
         <LoadingState />
       ) : error ? (
         <ErrorState />
-      ) : (
+      ) : posts.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
           {posts.map((post) => {
             return <PostCard key={post.id} {...post} />;
           })}
         </div>
+      ) : (
+        <NoData />
       )}
     </div>
   );
--- src/store/postStore.ts
+++ src/store/postStore.ts
@@ -4,15 +4,18 @@ import { immer } from "zustand/middleware/immer";
 interface PostStore {
   currentPage: number;
   limit: number;
+  term: string;
   getTotalPages: () => number;
   setCurrentPages: (page: number) => void;
   setLimit: (amount: number) => void;
+  setTerm: (text: string) => void;
 }
 
 export const usePostStore = create<PostStore>()(
   immer((set, get) => ({
     currentPage: 1,
     limit: 10,
+    term: "",
     getTotalPages: () => Math.ceil(100 / get().limit),
     setCurrentPages: (page: number) =>
       set((state) => {
@@ -21,6 +24,10 @@ export const usePostStore = create<PostStore>()(
     setLimit: (amount: number) =>
       set((state) => {
         state.limit = amount;
+      }),
+    setTerm: (text: string) =>
+      set((state) => {
+        state.term = text;
       }),
   }))
 );
