ac814a294cefd43a77763f3f5d2716f2d9fa5dcd
miri <miri@goodsoft.io>
2025-08-26 17:48:22 +0900

    [데이터 통신 심화] error boundary

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,22 @@
 import { Suspense } from "react";
 import { axiosInstance } from "./api/axios";
 import Posts from "./components/Posts";
+import { ErrorBoundary } from "react-error-boundary";
+import Error from "./components/Error";
 
 async function fetchPost() {
-  const { data } = await axiosInstance.get("/posts");
+  const { data } = await axiosInstance.get("/post2s");
   return data; // promise 객체
 }
 
 export default function App() {
   return (
     <>
-      <Suspense fallback={<p>Loading...</p>}>
-        <Posts promise={fetchPost()} />
-      </Suspense>
+      <ErrorBoundary FallbackComponent={Error}>
+        <Suspense fallback={<p>Loading...</p>}>
+          <Posts promise={fetchPost()} />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }
