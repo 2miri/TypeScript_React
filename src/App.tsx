e776dcade66a438123b44a9e8129a59a531be7c1
miri <miri@goodsoft.io>
2025-08-26 15:31:52 +0900

    [데이터 통신 심화] useActionState 실습 - 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,22 @@
 import { useActionState } from "react";
 
 export default function App() {
-  const [count, formAction, isPending] = useActionState(async (count) => {
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-    return count + 1;
-  }, 0);
+  const [count, formAction, isPending] = useActionState(
+    async (count: number, formData: FormData) => {
+      await new Promise((resolve) => setTimeout(resolve, 2000));
+      const amount = Number(formData.get("amount"));
+      return count + amount;
+    },
+    0
+  );
   return (
     <>
-      <form action={formAction}>
+      <form>
         <h1>Count : {count}</h1>
-        <button type="submit">증가</button>
+        <input type="number" name="amount"></input>
+        <button type="submit" disabled={isPending} formAction={formAction}>
+          증가
+        </button>
         {isPending && <p>제출중...</p>}
       </form>
     </>
