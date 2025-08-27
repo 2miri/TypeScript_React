62491afdf94fd4df7ba08681c23b2da9dcdb9a3e
miri <miri@goodsoft.io>
2025-08-27 15:42:48 +0900

    [React Router v7] React Router v7 중첩 라우트와 인덱스 라우트

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import DashboardHome from "./pages/dashboard/DashboardHome";
+import Dashboard from "./pages/dashboard/Dashboard";
+import DashboardSetting from "./pages/dashboard/DashboardSetting";
 
 const router = createBrowserRouter([
   {
@@ -14,6 +17,28 @@ const router = createBrowserRouter([
   {
     path: "/post",
     element: <h1>Post Page</h1>,
+  },
+  {
+    path: "/dashboard",
+    Component: Dashboard,
+    children: [
+      {
+        // path: "", // /dashboard
+        index: true, // 인덱스 라우트 == path:""와 동일
+        Component: DashboardHome,
+      },
+      {
+        // children 속성은 / 기재하지 않음
+        path: "setting", // /dashboard/setting
+        Component: DashboardSetting,
+        children: [
+          {
+            path: "custom", // /dashboard/setting/custom
+            element: <h1>DashboardSetting Home</h1>,
+          },
+        ],
+      },
+    ],
   },
 ]);
 
