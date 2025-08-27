32a9f205cf75224eebd1deec55832742dc2ceae1
miri <miri@goodsoft.io>
2025-08-27 11:36:20 +0900

    [데이터 통신 심화] 영화 목록 렌더링 - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,9 @@
-import { Suspense } from "react";
-import { axiosInstance } from "./api/axios";
-import Posts from "./components/Posts";
-import { ErrorBoundary } from "react-error-boundary";
-import Error from "./components/Error";
-import Loading from "./components/Loading";
-
-async function fetchPost() {
-  const { data } = await axiosInstance.get("/posts");
-  return data; // promise 객체
-}
+import Movie from "./components/movies/Movie";
 
 export default function App() {
   return (
     <>
-      <ErrorBoundary FallbackComponent={Error}>
-        <Suspense fallback={<Loading />}>
-          <Posts promise={fetchPost()} />
-        </Suspense>
-      </ErrorBoundary>
+      <Movie />
     </>
   );
 }
--- src/api/axios.ts
+++ src/api/axios.ts
@@ -7,3 +7,13 @@ export const axiosInstance = axios.create({
     "Contnet-Type": "application/json",
   },
 });
+
+export const movieAxios = axios.create({
+  baseURL: "https://api.themoviedb.org/3/movie",
+  timeout: 5000,
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDBmYjg3MzcxM2UwMWY1NjA3ZWExMmY1MTA0OTU4NCIsIm5iZiI6MTc1NjI1NDY0NS4zMjQsInN1YiI6IjY4YWU1MWI1YTFmZGIzN2MxNzBmODg5ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.PIYPKzQLLDQCXc5rqgbpPqD1CL8vQsMoZqokuJgSZKk",
+  },
+});
