d443380907e8ccdbc0c3afd4414faeff88802a6e
miri <miri@goodsoft.io>
2025-08-27 15:50:13 +0900

    [React Router v7] React Router v7 레이아웃 라우트

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,37 +4,45 @@ import About from "./pages/About";
 import DashboardHome from "./pages/dashboard/DashboardHome";
 import Dashboard from "./pages/dashboard/Dashboard";
 import DashboardSetting from "./pages/dashboard/DashboardSetting";
+import Default from "./pages/layouts/Default";
+import DashboardLayout from "./pages/layouts/DashboardLayout";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    Component: Home,
-  },
-  {
-    path: "/about",
-    element: <About />,
-  },
-  {
-    path: "/post",
-    element: <h1>Post Page</h1>,
-  },
-  {
-    path: "/dashboard",
-    Component: Dashboard,
+    Component: Default,
     children: [
       {
-        // path: "", // /dashboard
-        index: true, // 인덱스 라우트 == path:""와 동일
-        Component: DashboardHome,
+        path: "/",
+        Component: Home,
       },
       {
-        // children 속성은 / 기재하지 않음
-        path: "setting", // /dashboard/setting
-        Component: DashboardSetting,
+        path: "/about",
+        element: <About />,
+      },
+      {
+        Component: DashboardLayout,
         children: [
           {
-            path: "custom", // /dashboard/setting/custom
-            element: <h1>DashboardSetting Home</h1>,
+            path: "/dashboard",
+            Component: Dashboard,
+            children: [
+              {
+                // path: "", // /dashboard
+                index: true, // 인덱스 라우트 == path:""와 동일
+                Component: DashboardHome,
+              },
+              {
+                // children 속성은 / 기재하지 않음
+                path: "setting", // /dashboard/setting
+                Component: DashboardSetting,
+                children: [
+                  {
+                    path: "custom", // /dashboard/setting/custom
+                    element: <h1>DashboardSetting Home</h1>,
+                  },
+                ],
+              },
+            ],
           },
         ],
       },
