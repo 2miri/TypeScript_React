2fbbc7101a03ebd1af8baedcceabe49f1743a302
miri <miri@goodsoft.io>
2025-08-27 15:30:13 +0900

    [React Router v7] React Router v7 설치

---
+++ src/routes/index.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/about",
+    element: <About />,
+  },
+]);
+
+export default function Router() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
