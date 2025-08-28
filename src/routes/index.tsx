733f92801399076b30f647c324a04e859a7d98eb
miri <miri@goodsoft.io>
2025-08-28 09:30:15 +0900

    [React Router v7] React Router v7 loader 속성으로 데이터 패칭

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import DashboardHome from "./pages/dashboard/DashboardHome";
-import Dashboard from "./pages/dashboard/Dashboard";
-import DashboardSetting from "./pages/dashboard/DashboardSetting";
-import Default from "./pages/layouts/Default";
-import Post from "./post/Post";
-import PostDetail from "./post/PostDetail";
-import Login from "./pages/auth/Login";
+import Post from "./pages/post/Post";
+import Default from "./layouts/Default";
+import PostLoader from "./pages/post/PostLoader";
+import { axiosInstance } from "../api/axios";
 
 const router = createBrowserRouter([
   {
@@ -18,57 +14,16 @@ const router = createBrowserRouter([
         Component: Home,
       },
       {
-        path: "/:locale?/about",
-        element: <About />,
+        path: "/post",
+        Component: Post,
+      },
+      {
+        path: "/post-loader",
+        Component: PostLoader,
         loader: async () => {
-          await new Promise((resolve) => setTimeout(resolve, 2000));
+          const { data } = await axiosInstance.get("/posts");
+          return data;
         },
-      },
-      {
-        path: "/login",
-        Component: Login,
-        action: async ({ request }) => {
-          const formData = await request.formData();
-          const email = formData.get("email");
-          const pw = formData.get("pw");
-          console.log(email, pw);
-
-          return { message: "로그인 성공" };
-        },
-      },
-      { path: "/post/*", Component: Post },
-      { path: "/post/:id/detail/:detail", Component: PostDetail },
-      {
-        path: "/group",
-        // Component: DashboardLayout,
-        children: [
-          {
-            path: "dashboard",
-            Component: Dashboard,
-            children: [
-              {
-                // path: "", // /dashboard
-                index: true, // 인덱스 라우트 == path:""와 동일
-                Component: DashboardHome,
-              },
-              {
-                // children 속성은 / 기재하지 않음
-                path: "setting", // /dashboard/setting
-                Component: DashboardSetting,
-                children: [
-                  {
-                    path: "custom", // /dashboard/setting/custom
-                    element: <h1>DashboardSetting Home</h1>,
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      {
-        path: "*",
-        element: <h1>404 Not Fount!</h1>,
       },
     ],
   },
